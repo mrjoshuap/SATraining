2c54458671112a1de38728d70267335012714503
Josh Preston <jdp.github@prestoncentral.com>
2015-08-10 19:25:18 -0400

    fixing deploy path on people.redhat.com
    should not have used tilde

--- prepare/deploy.sh
+++ prepare/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh people.redhat.com mkdir -p ~/public_html/atomic-training
+ssh people.redhat.com mkdir -p public_html/atomic-training
 
 test -f index.html && scp index.html people.redhat.com:~/public_html/atomic-training
 
