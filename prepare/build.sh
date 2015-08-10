95f946fc506a66105d4ab3881bf079ecc3d3f005
Josh Preston <jdp.github@prestoncentral.com>
2015-08-10 19:21:37 -0400

    build fixes
    * create log directory
    * build atomic-master

--- prepare/build.sh
+++ prepare/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Generate our cloud-init images
-type -a genisoimage >/dev/null 2>&1 && mkdir -p iso && for A_HOST in atomic-host-*; do
-	genisoimage -output iso/${A_HOST}-cidata.iso -volid cidata -joliet -rock ${A_HOST}/user-data ${A_HOST}/meta-data > ${A_HOST}-cidata.log 2>&1
+type -a genisoimage >/dev/null 2>&1 && mkdir -p iso log && for A_HOST in atomic-*; do
+	test -d ${A_HOST} && genisoimage -output iso/${A_HOST}-cidata.iso -volid cidata -joliet -rock ${A_HOST}/user-data ${A_HOST}/meta-data > log/${A_HOST}-cidata.log 2>&1
 done
 
 # Generate our index file
