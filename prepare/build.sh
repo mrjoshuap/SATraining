baa95b1965faafd1e564d88bc3b4e8001f5c3eb8
Josh Preston <jdp.github@prestoncentral.com>
2015-08-10 16:58:36 -0400

    renaming build script
    nobody cares what we are building, just that we build.

---
+++ prepare/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Generate our cloud-init images
+type -a genisoimage >/dev/null 2>&1 && for A_HOST in atomic-host-*; do
+	pushd "${A_HOST}"
+	genisoimage -output ../${A_HOST}-cidata.iso -volid cidata -joliet -rock user-data meta-data
+	popd
+done
+
+# Generate our index file
+type -a markdown >/dev/null 2>&1 && {
+  cat header.html
+  markdown index.md
+  cat footer.html
+} > index.html
