4654acfeeef562b11a06883e5fbea628374e471d
Josh Preston <jdp.github@prestoncentral.com>
2015-08-10 19:16:31 -0400

    iso output cleanup
    iso images moved to a subdirectory for readability purposes

--- prepare/build.sh
+++ prepare/build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Generate our cloud-init images
-type -a genisoimage >/dev/null 2>&1 && for A_HOST in atomic-host-*; do
-	pushd "${A_HOST}"
-	genisoimage -output ../${A_HOST}-cidata.iso -volid cidata -joliet -rock user-data meta-data
-	popd
+type -a genisoimage >/dev/null 2>&1 && mkdir -p iso && for A_HOST in atomic-host-*; do
+	genisoimage -output iso/${A_HOST}-cidata.iso -volid cidata -joliet -rock ${A_HOST}/user-data ${A_HOST}/meta-data > ${A_HOST}-cidata.log 2>&1
 done
 
 # Generate our index file
