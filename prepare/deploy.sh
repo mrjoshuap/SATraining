b7bfb260291e505880a15f07c3b13eccb393cd5e
Josh Preston <jdp.github@prestoncentral.com>
2015-08-10 18:57:45 -0400

    create web directory if needed

--- prepare/deploy.sh
+++ prepare/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ssh people.redhat.com mkdir -p ~/public_html/atomic-training
+
 test -f index.html && scp index.html people.redhat.com:~/public_html/atomic-training
 
 find . -iname *.iso -exec scp {} people.redhat.com:~/public_html/atomic-training \;
