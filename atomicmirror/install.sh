84562f0f22b03b2fdb713b4954d7a5e32d6a1d92
jpreston <jdp.github@prestoncentral.com>
2015-10-01 16:03:02 -0400

    fix sed output for service

--- atomicmirror/install.sh
+++ atomicmirror/install.sh
@@ -10,7 +10,7 @@ cp -pR ${CONFDIR}/repos ${HOST}/${CONFDIR}
 chroot ${HOST} /usr/bin/docker create -p 8000 -v ${CONFDIR}:${CONFDIR}/${NAME}:Z -v ${LOGDIR}:${LOGDIR}/${NAME}:Z -v ${DATADIR}:${DATDIR}/${NAME}:Z --name ${NAME} ${IMAGE}
 
 # Install systemd unit file for running container
-sed -e "s/NAME/${NAME}/g" /etc/systemd/system/atomicmirror_template.service ${HOST}/etc/systemd/system/atomicmirror_${NAME}.service
+sed -e "s/NAME/${NAME}/g" /etc/systemd/system/atomicmirror_template.service > ${HOST}/etc/systemd/system/atomicmirror_${NAME}.service
 
 # Enabled systemd unit file
 chroot ${HOST} /usr/bin/systemctl enable /etc/systemd/system/atomicmirror_${NAME}.service
