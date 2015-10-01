95a4b33cd9945a0403e31ec9ef54f90e14c796d9
jpreston <jdp.github@prestoncentral.com>
2015-10-01 16:09:44 -0400

    fixing log directives

--- atomicmirror/run.sh
+++ atomicmirror/run.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
 
-echo "--------------------------------------" | tee ${LOGDIR}/${NAME}.log
-env | tee ${LOGDIR}/${NAME}.log
-echo "--------------------------------------" | tee ${LOGDIR}/${NAME}.log
+{
+  echo "--------------------------------------"
+  env
+  echo "--------------------------------------"
 
-if [ "${OSTREE_ENABLE_REPOS}x" = "x" ]; then
-  echo "ERROR: No ostree repositories specified!" | tee ${LOGDIR}/${NAME}.log
-  exit -1
-fi
-
-for OSTREE_REPO in ${OSTREE_ENABLE_REPOS}; do
-
-  if [ ! -f "${CONFDIR}/repos/${OSTREE_REPO}.repo" ]; then
-    echo "ERROR: No repository configuration found for [${OSTREE_REPO}]!" | tee ${LOGDIR}/${NAME}.log
-    continue
-  else
-    echo "INFO: Mirroring [${OSTREE_REPO}]" | tee ${LOGDIR}/${NAME}.log
+  if [ "${OSTREE_ENABLE_REPOS}x" = "x" ]; then
+    echo "ERROR: No ostree repositories specified!"
+    exit -1
   fi
 
-  unset OSTREE_GPG_VERIFY OSTREE_ORIGIN_URL OSTREE_ORIGIN_REF
-  source "${CONFDIR}/repos/${OSTREE_REPO}.repo"
+  for OSTREE_REPO in ${OSTREE_ENABLE_REPOS}; do
 
-  if [[ ! -d "${DATADIR}/${NAME}/${OSTREE_REPO}" && ! -f "${DATADIR}/${NAME}/${OSTREE_REPO}/config" ]]; then
-    mkdir -p "${DATADIR}/${NAME}/${OSTREE_REPO}"
-    ostree --repo="${DATADIR}/${NAME}/${OSTREE_REPO}" init --mode=archive-z2 | tee ${LOGDIR}/${NAME}.log
-    ostree --repo="${DATADIR}/${NAME}/${OSTREE_REPO}" remote add --set=gpg-verify=${OSTREE_GPG_VERIFY} origin ${OSTREE_ORIGIN_URL} | tee ${LOGDIR}/${NAME}.log
-  fi
+    if [ ! -f "${CONFDIR}/repos/${OSTREE_REPO}.repo" ]; then
+      echo "ERROR: No repository configuration found for [${OSTREE_REPO}]!"
+      continue
+    else
+      echo "INFO: Mirroring [${OSTREE_REPO}]"
+    fi
 
-  ostree --repo="${DATADIR}/${NAME}/${OSTREE_REPO}" pull origin ${OSTREE_ORIGIN_REF} | tee ${LOGDIR}/${NAME}.log \
-  && ostree --repo="${DATADIR}/${NAME}/${OSTREE_REPO}" fsck | tee ${LOGDIR}/${NAME}.log \
-  && ostree --repo="${DATADIR}/${NAME}/${OSTREE_REPO}" refs | tee ${LOGDIR}/${NAME}.log \
-  && ostree --repo="${DATADIR}/${NAME}/${OSTREE_REPO}" summary -u | tee ${LOGDIR}/${NAME}.log \
-  || echo "ERROR: Failed Mirroring ostree repository [${OSTREE_REPO}]" | tee ${LOGDIR}/${NAME}.log
+    unset OSTREE_GPG_VERIFY OSTREE_ORIGIN_URL OSTREE_ORIGIN_REF
+    source "${CONFDIR}/repos/${OSTREE_REPO}.repo"
 
-done
+    if [[ ! -d "${DATADIR}/${NAME}/${OSTREE_REPO}" && ! -f "${DATADIR}/${NAME}/${OSTREE_REPO}/config" ]]; then
+      mkdir -p "${DATADIR}/${NAME}/${OSTREE_REPO}"
+      ostree --repo="${DATADIR}/${NAME}/${OSTREE_REPO}" init --mode=archive-z2
+      ostree --repo="${DATADIR}/${NAME}/${OSTREE_REPO}" remote add --set=gpg-verify=${OSTREE_GPG_VERIFY} origin ${OSTREE_ORIGIN_URL}
+    fi
 
-echo -n "Starting polipo..." | tee ${LOGDIR}/${NAME}.log \
-  && polipo | tee ${LOGDIR}/${NAME}.log \
-    && echo "ok" | tee ${LOGDIR}/${NAME}.log \
-    || echo "failed" | tee ${LOGDIR}/${NAME}.log
+    echo "Pulling [${OSTREE_REPO}]" && ostree --repo="${DATADIR}/${NAME}/${OSTREE_REPO}" pull origin ${OSTREE_ORIGIN_REF} \
+    && echo "Checking [${OSTREE_REPO}]"&& ostree --repo="${DATADIR}/${NAME}/${OSTREE_REPO}" fsck \
+    && ostree --repo="${DATADIR}/${NAME}/${OSTREE_REPO}" refs \
+    && ostree --repo="${DATADIR}/${NAME}/${OSTREE_REPO}" summary -u \
+    || echo "ERROR: Failed Mirroring ostree repository [${OSTREE_REPO}]"
 
-echo "Starting SimpleHTTPServer..." | tee ${LOGDIR}/${NAME}.log \
-  && python -m SimpleHTTPServer | tee ${LOGDIR}/${NAME}_access.log
+  done
+
+  echo -n "Starting polipo..." \
+    && polipo \
+      && echo "ok" \
+      || echo "failed"
+} | tee -a ${LOGDIR}/${NAME}.log
+
+echo "Starting SimpleHTTPServer..." \
+  && python -m SimpleHTTPServer | tee -a ${LOGDIR}/${NAME}_access.log
