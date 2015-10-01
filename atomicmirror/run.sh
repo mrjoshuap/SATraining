47720be7d83a7f93024f5ea5d6c11489a18a0633
jpreston <jdp.github@prestoncentral.com>
2015-10-01 10:55:42 -0400

    updates for atomic mirror
    now uses atomic friendly directories
    outputs a log file

--- atomicmirror/run.sh
+++ atomicmirror/run.sh
@@ -1,41 +1,40 @@
 #!/bin/bash
 
-OSTREE_ENABLE_REPOS=$*
-
 if [ "${OSTREE_ENABLE_REPOS}x" = "x" ]; then
-  echo "ERROR: No ostree repositories specified!"
+  echo "ERROR: No ostree repositories specified!" | tee ${LOGDIR}/atomicmirror.log
   exit -1
 fi
 
 for OSTREE_REPO in ${OSTREE_ENABLE_REPOS}; do
 
-  if [ ! -f "/home/working/repos/${OSTREE_REPO}.repo" ]; then
-    echo "ERROR: No repository configuration found for [${OSTREE_REPO}]!"
+  if [ ! -f "${CONFDIR}/repos/${OSTREE_REPO}.repo" ]; then
+    echo "ERROR: No repository configuration found for [${OSTREE_REPO}]!" | tee ${LOGDIR}/atomicmirror.log
     continue
   else
-    echo "INFO: Mirroring [${OSTREE_REPO}]"
+    echo "INFO: Mirroring [${OSTREE_REPO}]" | tee ${LOGDIR}/atomicmirror.log
   fi
 
   unset OSTREE_GPG_VERIFY OSTREE_ORIGIN_URL OSTREE_ORIGIN_REF
-  source "/home/working/repos/${OSTREE_REPO}.repo"
+  source "${CONFDIR}/repos/${OSTREE_REPO}.repo"
 
-  pushd /srv/rpm-ostree/
-
-  if [ ! -d "/srv/rpm-ostree/${OSTREE_REPO}" ]; then
-    mkdir -p "/srv/rpm-ostree/${OSTREE_REPO}"
-    ostree --repo="/srv/rpm-ostree/${OSTREE_REPO}" init --mode=archive-z2
-    ostree --repo="/srv/rpm-ostree/${OSTREE_REPO}" remote add --set=gpg-verify=${OSTREE_GPG_VERIFY} origin ${OSTREE_ORIGIN_URL}
+  if [ ! -d "${DATADIR}/${OSTREE_REPO}" ]; then
+    mkdir -p "${DATADIR}/${OSTREE_REPO}"
+    ostree --repo="${DATADIR}/${OSTREE_REPO}" init --mode=archive-z2 | tee ${LOGDIR}/atomicmirror.log
+    ostree --repo="${DATADIR}/${OSTREE_REPO}" remote add --set=gpg-verify=${OSTREE_GPG_VERIFY} origin ${OSTREE_ORIGIN_URL} | tee ${LOGDIR}/atomicmirror.log
   fi
 
-  ostree --repo="/srv/rpm-ostree/${OSTREE_REPO}" pull origin ${OSTREE_ORIGIN_REF} \
-  && ostree --repo="/srv/rpm-ostree/${OSTREE_REPO}" fsck \
-  && ostree --repo="/srv/rpm-ostree/${OSTREE_REPO}" refs \
-  && ostree --repo="/srv/rpm-ostree/${OSTREE_REPO}" summary -u \
-  || echo "ERROR: Failed Mirroring ostree repository [${OSTREE_REPO}]"
+  ostree --repo="${DATADIR}/${OSTREE_REPO}" pull origin ${OSTREE_ORIGIN_REF} | tee ${LOGDIR}/atomicmirror.log \
+  && ostree --repo="${DATADIR}/${OSTREE_REPO}" fsck | tee ${LOGDIR}/atomicmirror.log \
+  && ostree --repo="${DATADIR}/${OSTREE_REPO}" refs | tee ${LOGDIR}/atomicmirror.log \
+  && ostree --repo="${DATADIR}/${OSTREE_REPO}" summary -u | tee ${LOGDIR}/atomicmirror.log \
+  || echo "ERROR: Failed Mirroring ostree repository [${OSTREE_REPO}]" | tee ${LOGDIR}/atomicmirror.log
 
 done
 
-echo -n "Starting polipo..." && polipo && echo "ok" || echo "failed"
-echo -n "Starting SimpleHTTPServer..." && python -m SimpleHTTPServer && echo "ok" || echo "failed"
+echo -n "Starting polipo..." | tee ${LOGDIR}/atomicmirror.log \
+  && polipo | tee ${LOGDIR}/atomicmirror.log \
+    && echo "ok" | tee ${LOGDIR}/atomicmirror.log \
+    || echo "failed" | tee ${LOGDIR}/atomicmirror.log
 
-popd
+echo "Starting SimpleHTTPServer..." | tee ${LOGDIR}/atomicmirror.log \
+  && python -m SimpleHTTPServer
