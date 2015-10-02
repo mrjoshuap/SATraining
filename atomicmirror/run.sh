e3515f3cc0a5475d8fb842672c89dad92e8bf088
Josh Preston <jdp.github@prestoncentral.com>
2015-10-01 23:49:46 -0400

    fixing repository by taking out the name in running path

--- atomicmirror/run.sh
+++ atomicmirror/run.sh
@@ -22,16 +22,16 @@
     unset OSTREE_GPG_VERIFY OSTREE_ORIGIN_URL OSTREE_ORIGIN_REF
     source "${CONFDIR}/repos/${OSTREE_REPO}.repo"
 
-    if [[ ! -d "${DATADIR}/${NAME}/${OSTREE_REPO}" && ! -f "${DATADIR}/${NAME}/${OSTREE_REPO}/config" ]]; then
-      mkdir -p "${DATADIR}/${NAME}/${OSTREE_REPO}"
-      ostree --repo="${DATADIR}/${NAME}/${OSTREE_REPO}" init --mode=archive-z2
-      ostree --repo="${DATADIR}/${NAME}/${OSTREE_REPO}" remote add --set=gpg-verify=${OSTREE_GPG_VERIFY} origin ${OSTREE_ORIGIN_URL}
+    if [[ ! -d "${DATADIR}/${OSTREE_REPO}" && ! -f "${DATADIR}/${OSTREE_REPO}/config" ]]; then
+      mkdir -p "${DATADIR}/${OSTREE_REPO}"
+      ostree --repo="${DATADIR}/${OSTREE_REPO}" init --mode=archive-z2
+      ostree --repo="${DATADIR}/${OSTREE_REPO}" remote add --set=gpg-verify=${OSTREE_GPG_VERIFY} origin ${OSTREE_ORIGIN_URL}
     fi
 
-    echo "Pulling [${OSTREE_REPO}]" && ostree --repo="${DATADIR}/${NAME}/${OSTREE_REPO}" pull origin ${OSTREE_ORIGIN_REF} \
-    && echo "Checking [${OSTREE_REPO}]"&& ostree --repo="${DATADIR}/${NAME}/${OSTREE_REPO}" fsck \
-    && ostree --repo="${DATADIR}/${NAME}/${OSTREE_REPO}" refs \
-    && ostree --repo="${DATADIR}/${NAME}/${OSTREE_REPO}" summary -u \
+    echo "Pulling [${OSTREE_REPO}]" && ostree --repo="${DATADIR}/${OSTREE_REPO}" pull origin ${OSTREE_ORIGIN_REF} \
+    && echo "Checking [${OSTREE_REPO}]"&& ostree --repo="${DATADIR}/${OSTREE_REPO}" fsck \
+    && ostree --repo="${DATADIR}/${OSTREE_REPO}" refs \
+    && ostree --repo="${DATADIR}/${OSTREE_REPO}" summary -u \
     || echo "ERROR: Failed Mirroring ostree repository [${OSTREE_REPO}]"
 
   done
